d006e61771d4998f59d2729d3a45dcdd7d527d90
Ryan Duan <duanpanda@gmail.com>
2017-09-07 22:15:41 -0400

    Java MD5 and SHA-2

---
+++ HackerRank/JavaSHA-2.java
@@ -0,0 +1,17 @@
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.security.MessageDigest;
+
+public class JavaSHA-2 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner input = new Scanner(System.in);
+        MessageDigest m = MessageDigest.getInstance("SHA-256");
+        m.reset();
+        m.update(input.nextLine().getBytes());
+        for (byte i : m.digest()) {
+            System.out.print(String.format("%02x", i));
+        }
+        System.out.println();
+    }
+}
