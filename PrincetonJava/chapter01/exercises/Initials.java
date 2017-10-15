cc99b5136107f9fcd31508040a2a3462e364001c
Ryan Duan <duanpanda@gmail.com>
2017-10-14 23:24:15 -0400

    complete section 1.1

---
+++ PrincetonJava/chapter01/exercises/Initials.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+ *  Compilation:  javac Initials.java
+ *  Execution:    java Initials
+ *
+ *  Prints the initials RD.
+ *
+ ******************************************************************************/
+
+public class Initials {
+
+    public static void main(String[] args) {
+        System.out.println("*********        **********     ");
+        System.out.println("**      ***      **        **   ");
+        System.out.println("**     ***       **         **  ");
+        System.out.println("**  ***          **          ** ");
+        System.out.println("*****            **          ** ");
+        System.out.println("**  ***          **          ** ");
+        System.out.println("**    ***        **         **  ");
+        System.out.println("**      ***      **        **   ");
+        System.out.println("**        ***    **********     ");
+    }
+
+}
